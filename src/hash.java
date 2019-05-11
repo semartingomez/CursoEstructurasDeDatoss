9baa35104db17b8d987ffd13b3921e4a27fcbc25
general <general@Labs-079.poligran.edu.co>
2019-05-11 09:17:57 -0500

    efsc

--- src/hash.java
+++ src/hash.java
@@ -1,4 +1,21 @@
 
 public class hash {
-
+public class HashNode implements Node{
+public int key;	
+public List data;
+public HashNode next;
 }
+	
+	private int subsets=100;
+	private List keys= new List();
+	public hash() {
+
+	}
+	public int hash_fuction(int id) {
+		return id %subsets;
+	}
+	public void insert (int id) {
+		
+	}
+}
+
