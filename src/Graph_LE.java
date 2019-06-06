1a6ff06d03e7f5b1124d7a1ac8773c8c2fd2c274
MARTIN GOMEZ <MARTIN GOMEZ@DESKTOP-NJ48I4O>
2019-06-06 12:47:14 -0500

    proyecto

--- src/Graph_LE.java
+++ src/Graph_LE.java
@@ -1,55 +1,72 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 
-public class Graph_LE implements Node{
-	
-	private int Data;
-	
-	int label;
-	Node next;
-	List nodes = new List();
-	List edges = new List();
-	boolean visited = false;
-	
-	Node MostImportant=null;
-
-	public Graph_LE(int Data, int label) {
-		this.Data = Data;
-		this.label = label;
-	}
-	public Graph_LE() {
+public class Graph implements Node  {
 		
-	}
+		private int Data;
+		
+		int label;
+		Node next;
+		List nodes = new List();
+		List edges = new List();
+		boolean visited = false;
+		
+		Node MostImportant=null;
 
-	
-	public void insert_node() {
-		nodes.insertAtEnd(next);
-	}
+		public Graph(int Data, int label) {
+			this.Data = Data;
+			this.label = label;
+		}
+		public Graph() {
+			
+		}
 
-	public void delete_node() {
-		Graph_LE search = new Graph_LE(0, label);
-		for (int i = 0; i < nodes.length(); i++)
-			if (nodes.get(i).isEqual(search)) {
-				nodes.deleteAtIndex(i);
-				break;
+		
+		public void insert_node() {
+			nodes.insertAtEnd(next);
+		}
+
+		public void delete_node() {
+			Graph search = new Graph(0, label);
+			for (int i = 0; i < nodes.length(); i++)
+				if (nodes.get(i).isEqual(search)) {
+					nodes.deleteAtIndex(i);
+					break;
+				}
+				
+		}
+
+		public void insert_edge(int label1, int label2) {
+			Graph search = new Graph(0, label1);
+			for (int i = 0; i < nodes.length(); i++)
+				if (nodes.get(i).isEqual(search)) {
+					edges.insertAtEnd(search);
+					break;
+				}
+
+			search = new Graph(0, label2);
+			for (int i = 0; i < nodes.length(); i++)
+				if (nodes.get(i).isEqual(search)) {
+					edges.insertAtEnd(search);
+					break;
+				
+		
+		}
+			
+		}
+		public static void main(String[] args)
+		{
+			
+			
+			try {
+				FileReader fr=new FileReader("datos proyecto.txt");
+				BufferedReader br =new BufferedReader (fr);
 			}
-
-	}
-
-	public void insert_edge(int label1, int label2) {
-		Graph_LE search = new Graph_LE(0, label1);
-		for (int i = 0; i < nodes.length(); i++)
-			if (nodes.get(i).isEqual(search)) {
-				edges.insertAtEnd(search);
-				break;
+			catch(Exception ex) {
+				ex.printStackTrace();
 			}
-
-		search = new Graph_LE(0, label2);
-		for (int i = 0; i < nodes.length(); i++)
-			if (nodes.get(i).isEqual(search)) {
-				edges.insertAtEnd(search);
-				break;
-			}
-
-	}
-
-u
-	}
+			
+		}
+		}
+			
+		
